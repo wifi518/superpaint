97f546d7246933e2a82e7d413c673661b08dab4c
Alex Wifi <wifi518@1av.at>
2019-06-05 11:20:33 +0200

    0.1.0 done

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+console.log( 'JS loaded' );
+document.addEventListener( 'deviceready', function() {
+  console.log( 'Device ready' );
+  $( document ).ready( function() {
+    console.log( 'DOM ready, alles ready...' );
+    navigator.geolocation.getCurrentPosition( function( coords ) {
+        $('<div>').html(coords.latitude + '/' + coords.longitude).appendTo( 'body' );
+    },function() {
+        $('<div>').html('Error no position').appendTo( 'body');
+    })
+
+  });
+});
